086a2f90cdc22cf8079039ffca42f0da87cef1f2
Sahil <sahil1372001@gmail.com>
2024-02-21 21:58:16 +0530

    Added CRUD Operation API

---
+++ connection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+require('dotenv').config()
+
+var mySqlConnection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER ,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+mySqlConnection.connect((err) => {
+    if(err){
+        console.log("error in Db connection" + JSON.stringify(err, undefined, 2));
+    }
+    else {
+        console.log("Db connection successfull");
+    }
+})
+
+module.exports = mySqlConnection;
